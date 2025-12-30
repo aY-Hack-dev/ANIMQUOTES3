25157ba8cb23eaf3fa3df3f160b989e9398e103e
aY-Hack-dev <abimaelyameogo@gmail.com>
2025-12-30 00:30:58 +0000

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,13 +1,10 @@
-// Vérifie si l'utilisateur avait déjà choisi le dark mode
-const isDark = localStorage.getItem('dark-mode') === 'true';
-if(isDark) document.body.classList.add('dark-mode');
-
-// Toggle bouton global
+if(localStorage.getItem('dark-mode') === 'true') {
+  document.body.classList.add('dark-mode');
+}
 const themeToggle = document.getElementById('theme-toggle');
 if(themeToggle){
     themeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
-        // Sauvegarde le choix
         localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
     });
 }
